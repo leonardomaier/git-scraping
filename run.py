a7b2bab6e6482def967d41dcd68a588bf05fbdd4
Leonardo Maier <leonarmaier@gmail.com>
2020-08-15 23:37:55 -0300

    Cria arquivos de saída

--- run.py
+++ run.py
@@ -5,13 +5,6 @@ import requests
 base_url = 'https://github.com/'
 
 
-# Parse request content
-def parse_html(text):
-
-    return BeautifulSoup(text, 'html.parser')
-
-
-# Extract file and directory links
 def get_dir_links(soup):
 
     links = []
@@ -57,9 +50,7 @@ def get_files_links_recursively(url, links, output):
 
         for folder in folders:
 
-            r = requests.get(url + folder)
-
-            soup = parse_html(r.text)
+            soup = request_and_parse(url + folder)
 
             links = get_dir_links(soup)
 
@@ -68,24 +59,62 @@ def get_files_links_recursively(url, links, output):
     return output
 
 
+def request_and_parse(url):
+
+    response = requests.get(url)
+
+    return BeautifulSoup(response.text, 'html.parser')
+
+
 with open('repositories.txt') as repositories:
 
     lines = repositories.read().splitlines()
 
     for repo_url in lines:
 
+        print('-------------- ' + repo_url + ' --------------')
+
+        output_file = open(
+            'outputs/' + repo_url.replace('/', '_') + '.txt', 'w')
+
         output = []
 
         url = base_url + repo_url
 
-        r = requests.get(url)
-
-        soup = parse_html(r.text)
+        soup = request_and_parse(url)
 
         links = get_dir_links(soup)
 
         files = get_files_links_recursively(url, links, output)
 
+        extension_data = {}
+
         for file_dir in files:
 
-            print(file_dir)
+            soup = request_and_parse(url + file_dir)
+
+            filename = soup.find('strong', class_='final-path').text
+
+            file_ext = filename.split('.')[1]
+
+            file_data_div = soup.select('div.Box-header.py-2 .text-mono')
+
+            file_data_arr = file_data_div[0].text.strip().split(' ')
+
+            print(filename)
+
+            if file_ext not in extension_data:
+
+                extension_data[file_ext] = {
+                    'total_lines': int(file_data_arr[0])
+                }
+
+            else:
+
+                extension_data[file_ext]['total_lines'] += int(file_data_arr[0])
+
+        output_file.write('Extensão   |   Linhas   |   Bytes\n')
+
+        for ext in extension_data:
+
+            output_file.write(ext + '    | ' + str(extension_data[ext]['total_lines']) + '   |\n')
